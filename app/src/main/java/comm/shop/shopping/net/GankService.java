8b4cf2f4095f14f3b1369dd0d00626ff37591aed
tint <18285282@qq.com>
2018-11-24 10:58:20 +0800

    项目基础搭建

---
+++ app/src/main/java/comm/shop/shopping/net/GankService.java
@@ -0,0 +1,19 @@
+package comm.shop.shopping.net;
+
+import comm.shop.shopping.model.GankResults;
+
+import io.reactivex.Flowable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by wanglei on 2016/12/31.
+ */
+
+public interface GankService {
+
+    @GET("data/{type}/{number}/{page}")
+    Flowable<GankResults> getGankData(@Path("type") String type,
+                                      @Path("number") int pageSize,
+                                      @Path("page") int pageNum);
+}
