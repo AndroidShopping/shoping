b714f0aacac07a093052d0d157200e514273fe37
tint <18285282@qq.com>
2018-11-27 22:40:46 +0800

    去除串口通信部分

--- app/src/main/java/comm/shop/shopping/App.java
+++ app/src/main/java/comm/shop/shopping/App.java
@@ -27,63 +27,16 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = this;
-        SerialPortFinder finder = new SerialPortFinder();
-        String[] allDevicesPath = finder.getAllDevicesPath();
-        ICT104 = new SerialPortUtil();
-        if (allDevicesPath != null) {
-            ICT104.initSerialPort(allDevicesPath[0], SerialPortUtil.DEFAULT_BO_TE, 0);
-        } else {
-            ICT104.initSerialPort("/dev/ttyS4", SerialPortUtil.DEFAULT_BO_TE, 0);
-        }
+//        SerialPortFinder finder = new SerialPortFinder();
+//        String[] allDevicesPath = finder.getAllDevicesPath();
+//        ICT104 = new SerialPortUtil();
+//        if (allDevicesPath != null) {
+//            ICT104.initSerialPort(allDevicesPath[0], SerialPortUtil.DEFAULT_BO_TE, 0);
+//        } else {
+//            ICT104.initSerialPort("/dev/ttyS4", SerialPortUtil.DEFAULT_BO_TE, 0);
+//        }
 
 
-        XApi.registerProvider(new NetProvider() {
-
-            @Override
-            public Interceptor[] configInterceptors() {
-                return new Interceptor[0];
-            }
-
-            @Override
-            public void configHttps(OkHttpClient.Builder builder) {
-
-            }
-
-            @Override
-            public CookieJar configCookie() {
-                return null;
-            }
-
-            @Override
-            public RequestHandler configHandler() {
-                return null;
-            }
-
-            @Override
-            public long configConnectTimeoutMills() {
-                return 0;
-            }
-
-            @Override
-            public long configReadTimeoutMills() {
-                return 0;
-            }
-
-            @Override
-            public boolean configLogEnable() {
-                return true;
-            }
-
-            @Override
-            public boolean handleError(NetError error) {
-                return false;
-            }
-
-            @Override
-            public boolean dispatchProgressEnable() {
-                return false;
-            }
-        });
     }
 
     public static Context getContext() {
@@ -93,7 +46,7 @@ public class App extends Application {
     @Override
     public void onTerminate() {
         //程序终止关闭串口
-        ICT104.closeSerialPort();
+//        ICT104.closeSerialPort();
         super.onTerminate();
     }
 
